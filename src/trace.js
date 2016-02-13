27d48da04d2e5ab9245b56ddafaf6d4d69aab2b1
Lewuathe <lewuathe@me.com>
2016-02-13 22:44:46 +0900

    Fix path issue

--- src/trace.js
+++ src/trace.js
@@ -5,14 +5,14 @@ import logger from './logger';
 import ProgressBar from 'progress';
 
 let config = require(process.argv[2]);
+let buildDir = process.argv[3];
 let appName = config['app_name'];
 let appId = config['app_id'];
 let restApiKey = config['restapi_key'];
 let masterKey = config['master_key'];
 let classes = config['classes'];
 
-rimraf.sync('./build/' + appName, {});
-fs.mkdirSync('./build/' + appName);
+fs.mkdirSync(buildDir + '/' + appName);
 
 let bar = new ProgressBar('  Tracing [:bar] :percent :etas', {
     complete: '=',
@@ -25,7 +25,7 @@ let bar = new ProgressBar('  Tracing [:bar] :percent :etas', {
  * @param klass
  */
 function trace(endpoint, klass, env) {
-    var output = './build/' + appName + '/' + klass + '.' + env + '.json';
+    var output = buildDir + '/' + appName + '/' + klass + '.' + env + '.json';
     var cmd = ['curl',
         '-H',
         '"X-Parse-REST-API-Key: ' + restApiKey + '"',
--- src/verify.js
+++ src/verify.js
@@ -7,12 +7,14 @@ let config = require('../config/api.json');
 
 import { AssertClass } from './AssertClass.js';
 
+let buildDir = process.argv[3];
+
 var appName = config['app_name'];
 var classes = config['classes'];
 
 for (let klass of classes) {
-    var local = require('../build/' + appName + '/' + klass + '.local.json');
-    var parse = require('../build/' + appName + '/' + klass + '.parse.json');
+    var local = require(buildDir + '/' + appName + '/' + klass + '.local.json');
+    var parse = require(buildDir + '/' + appName + '/' + klass + '.parse.json');
     assert.equal(local.results.length, parse.results.length);
     for (var i = 0; i < local.results.length; i++) {
         AssertClass.verify(local.results[i], parse.results[i]);
