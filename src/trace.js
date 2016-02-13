d014872b0cee9ddaa489f251a2a7c576ea3bd651
Lewuathe <lewuathe@me.com>
2016-02-13 21:04:41 +0900

    Progress bar

--- src/trace.js
+++ src/trace.js
@@ -1,40 +1,54 @@
-var fs = require('fs');
-var loading = require('loading-indicator');
-var logger = require('./logger');
-var config = require('../config/api.json');
-const child_process = require('child_process');
+const exec = require('child_process').exec;
+import fs from 'fs';
+import rimraf from 'rimraf';
+import logger from './logger';
+import ProgressBar from 'progress';
 
+var config = require('../config/api.json');
 var appName = config['app_name'];
 var appId = config['app_id'];
 var restApiKey = config['restapi_key'];
 var masterKey = config['master_key'];
 var classes = config['classes'];
 
+rimraf.sync('./build/' + appName, {});
 fs.mkdirSync('./build/' + appName);
 
+var bar = new ProgressBar('  Tracing [:bar] :percent :etas', {
+    complete: '=',
+    incomplete: ' ',
+    width: 40,
+    total: classes.length * 2
+});
 /**
  * Trace the API provided by specific class in Parse
  * @param klass
  */
-function trace(endpoint, klass, isParse) {
-    var output = isParse ? './build/' + appName + '/' + klass + '.parse.json' : './build/' + appName + '/' + klass + '.local.json';
+function trace(endpoint, klass, env) {
+    var output = './build/' + appName + '/' + klass + '.' + env + '.json';
     var cmd = ['curl',
-    //'--trace',
-    //'-',
-    '-H', '"X-Parse-REST-API-Key: ' + restApiKey + '"', '-H', '"X-Parse-Application-Id: ' + appId + '"', '-H', '"X-Parse-Master-Key: ' + masterKey + '"', '-G', '--data-urlencode', '\'order=objectId\'', '--data-urlencode', '\'limit=100\'', endpoint + 'classes/' + klass,
-    //'|',
-    //'./node_modules/.bin/curl-trace-parser',
-    // '--blueprint',
+        '-H',
+        '"X-Parse-REST-API-Key: ' + restApiKey + '"',
+        '-H', '"X-Parse-Application-Id: ' + appId + '"',
+        '-H', '"X-Parse-Master-Key: ' + masterKey + '"',
+        '-G', '--data-urlencode',
+        '\'order=objectId\'',
+        '--data-urlencode',
+        '\'limit=100\'',
+        endpoint + 'classes/' + klass,
     '>', output];
-    var timer = loading.start('Tracing... ' + klass);
-    logger.debug(cmd);
-    child_process.execSync(cmd.join(' '), {});
-    logger.info(klass + ' has been finished');
-    loading.stop(timer);
+    exec(cmd.join(' '), (err, stdout, stderr) => {
+        if (err) {
+            logger.error(err);
+        }
+        bar.tick(1);
+    });
+    //logger.info(klass + '(' + env + ') has been finished');
 }
+
 
 for (var i = 0; i < classes.length; i++) {
     // Trace all APIs provided target parse server.
-    trace(config['endpoint'], classes[i], false);
-    trace(config['parse_endpoint'], classes[i], true);
+    trace(config['endpoint'], classes[i], 'local');
+    trace(config['parse_endpoint'], classes[i], 'parse');
 }
