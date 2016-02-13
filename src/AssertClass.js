0ab635ba76981be86090ea798fe4520956f5cb67
Lewuathe <lewuathe@me.com>
2016-02-13 21:19:03 +0900

    Colorful UI

--- src/AssertClass.js
+++ src/AssertClass.js
@@ -1,15 +1,20 @@
 "use strict";
 
-var assert = require('assert');
-var logger = require('./logger');
+import assert from 'assert';
+import colors from 'colors';
+import logger from './logger';
 
 export class AssertClass {
     constructor() {}
 
     static verify(class1, class2) {
         for (let k of Object.keys(class1)) {
-            assert.deepEqual(class1[k], class2[k]);
+            try {
+                assert.deepEqual(class1[k], class2[k]);
+            } catch (e) {
+                logger.warn('Error '.red + ' ' + class1[k] + '!=' + class2[k]);
+            }
+            //console.log('okay'.green + ' ' + class1[k] + '=' + class2[k]);
         }
-
     }
 }
--- src/spec.js
+++ src/spec.js
@@ -1,21 +1,21 @@
-var fs              = require('fs');
-var assert          = require('assert');
-var loading         = require('loading-indicator');
-var logger          = require('./logger');
-var config          = require('../config/api.json');
-const child_process = require('child_process');
+import fs from 'fs';
+import assert from 'assert';
+import colors from 'colors';
+import logger from './logger';
+
+var config = require('../config/api.json');
 
 import { AssertClass } from './AssertClass.js';
 
 var appName = config['app_name'];
 var classes = config['classes'];
 
-for (var i = 0; i < classes.length; i++) {
-    var klass = classes[i];
+for (let klass of classes) {
     var local = require('../build/' + appName + '/' + klass + '.local.json');
     var parse = require('../build/' + appName + '/' + klass + '.parse.json');
     assert.equal(local.results.length, parse.results.length);
     for (var i = 0; i < local.results.length; i++) {
         AssertClass.verify(local.results[i], parse.results[i]);
     }
+    console.log('OK: '.green + klass.underline);
 }
